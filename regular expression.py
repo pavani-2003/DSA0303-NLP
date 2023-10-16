eaa424d38f41c332f2e34841c5b11018a7aae2b1
pavani-2003 <112242637+pavani-2003@users.noreply.github.com>
2023-10-16 12:26:03 +0530

    Add files via upload

--- regular expression.py
+++ regular expression.py
@@ -1,26 +1,26 @@
 import re
 
-def tokenize(text):
-    # Tokenize text using regular expression
-    tokens = re.findall(r'\b\w+\b', text)
-    return tokens
+# Sample text
+text = "Hello, my email is pavanni@gmail.com and my phone number is +1 (123) 456-7890."
 
-def find_emails(text):
-    # Find email addresses using regular expression
-    emails = re.findall(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b', text)
-    return emails
+# Define a regular expression pattern to match email addresses
+email_pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
 
-def find_dates(text):
-    # Find dates using regular expression (simple example)
-    dates = re.findall(r'\d{1,2}/\d{1,2}/\d{2,4}', text)
-    return dates
+# Use re.search() to find the first match in the text
+email_match = re.search(email_pattern, text)
 
-# Example Usage
-text = "Hello, my email is user@example.com. I'll meet you on 12/25/2023."
-tokens = tokenize(text)
-emails = find_emails(text)
-dates = find_dates(text)
+if email_match:
+    print("Email found:", email_match.group(0))
+else:
+    print("Email not found")
 
-print("Tokens:", tokens)
-print("Emails:", emails)
-print("Dates:", dates)
+# Define a regular expression pattern to match phone numbers
+phone_pattern = r'\+\d{1,3} \(\d{3}\) \d{3}-\d{4}'
+
+# Use re.findall() to find all matches in the text
+phone_matches = re.findall(phone_pattern, text)
+
+if phone_matches:
+    print("Phone numbers found:", phone_matches)
+else:
+    print("Phone numbers not found")
